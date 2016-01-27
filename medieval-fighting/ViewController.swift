d032c45a4e8816bd64629ab4e92323e8def8e856
Edward White <edwardwhite@outlook.com>
2016-01-27 00:48:58 -0600

    Basic functionality of game start button done

--- medieval-fighting/ViewController.swift
+++ medieval-fighting/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //vars and constants
+    let player1 = Player1(name: "Battle Orc", hp: 100, attackPower: 20)
+    let player2 = Player2(name: "Legionaire", hp: 100, attackPower: 20)
+    
+    @IBOutlet weak var player1Attack: UIButton!
+    @IBOutlet weak var player2Attack: UIButton!
+    @IBOutlet weak var startGame: UIButton!
+    @IBOutlet weak var printLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startGame(sender: UIButton) {
+        
+        //hide startgame button
+        startGame.hidden = true
+        //unhide attack buttons
+        player1Attack.hidden = false
+        player2Attack.hidden = false
+        
+        //play sound when pressed
+        
+    }
 
+    @IBAction func player1Attack(sender: UIButton) {
+        //attack
+        // send data to the bottom label
+        //hide player2 attack button for 2 seconds
+    }
+    @IBAction func player2Attack(sender: UIButton) {
+        //attack
+        //send info about attack to the bottom label
+        //hide player1 attack button for 2 seconds
+    }
 }
 
